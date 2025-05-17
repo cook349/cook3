015f25cf20643a09f2e3f64537d9db0f39a75687
MaiarHadi <mayarobaid850@gmail.com>
2025-05-17 11:56:36 +0300

    w

--- src/test/java/org/example/cook1/AcceptanceTest/acceptanceTest.java
+++ src/test/java/org/example/cook1/AcceptanceTest/acceptanceTest.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "cases",
+@CucumberOptions(features = "src/test/resources/cases",
         plugin = {"html:target/cucumber/wikipedia.html"},
         monochrome = true,
         snippets = SnippetType.CAMELCASE,
